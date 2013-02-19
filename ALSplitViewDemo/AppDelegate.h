f4c52970844feb537511205663bd50f762e37987
silvansky <v.silvansky@gmail.com>
2013-02-19 11:07:13 +0400

    Initial Commit

---
+++ ALSplitViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ALSplitViewDemo
+//
+//  Created by Valentine Gorshkov on 19.02.13.
+//  Copyright (c) 2013 silvansky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ALSplitViewDemo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ALSplitViewDemo
+//
+//  Created by Valentine Gorshkov on 19.02.13.
+//  Copyright (c) 2013 silvansky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
